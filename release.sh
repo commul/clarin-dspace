4ef720d64ff320e7af726c4d1cad5b8ac477c34b
egon w. stemle <egon.stemle@eurac.edu>
2021-01-11 23:27:31 +0100

    Automatically determine UFAL- and ERCC_VERSION; ask for confirmation; save VERSION

--- release.sh
+++ release.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -e
 
-ERCC_VERSION="${1:-latest}"
-UFAL_VERSION="$(git describe --tags --abbrev=0 $(git rev-list --tags --max-count=1 upstream/clarin))"
+# FIXME: run tests (at least, autocheck syntax...)
+# FIXME: make sure all changes are commited into the repository
+# FIXME: (automatically) increase .version
+
+ERCC_TAG="$(git describe --tags --abbrev=0 $(git rev-list --tags --max-count=1))"
+ERCC_VERSION="${ERCC_TAG:-latest}"
+UFAL_VERSION="$(git describe --tags --abbrev=0 $(git tag --list --sort="-creatordate" --merged clarin | head -n1))"
+VERSION="$UFAL_VERSION:$ERCC_VERSION"
+
 REPO_BASE="gitlab.inf.unibz.it:4567/commul/docker/clarin-dspace"
+REPO_URL="$REPO_BASE/dspace-app/$VERSION"
+
+echo "About to build: $REPO_URL"
+read -p "Press enter to continue"
 
 # export DOCKER_BUILDKIT=1
-REPO_URL="$REPO_BASE/dspace-app/$UFAL_VERSION:$ERCC_VERSION"
 docker build \
     --build-arg LABEL_COMMIT_HASH="$(git rev-parse --short HEAD)" \
     --build-arg LABEL_VERSION="${UFAL_VERSION}_${ERCC_VERSION}" \
@@ -16,3 +26,4 @@ docker build \
 docker push "$REPO_URL"
 
 echo "Make sure to change Dockerfile.dspace to point to: $REPO_URL"
+echo "$VERSION" > .version
