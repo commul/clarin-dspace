73265ea320322144f7d1ac41174d86fc6cf09f87
egon w. stemle <egon.stemle@eurac.edu>
2021-01-13 16:14:31 +0100

    Fix missing/wrongly named labels

--- release.sh
+++ release.sh
@@ -8,22 +8,26 @@ set -e
 ERCC_TAG="$(git describe --tags --abbrev=0 $(git rev-list --tags --max-count=1))"
 ERCC_VERSION="${ERCC_TAG:-latest}"
 UFAL_VERSION="$(git describe --tags --abbrev=0 $(git tag --list --sort="-creatordate" --merged clarin | head -n1))"
-VERSION="$UFAL_VERSION:$ERCC_VERSION"
 
-REPO_BASE="gitlab.inf.unibz.it:4567/commul/docker/clarin-dspace"
-REPO_URL="$REPO_BASE/dspace-app/$VERSION"
+REPO="https://github.com/commul/clarin-dspace"
+REGISTRY_BASE="gitlab.inf.unibz.it:4567/commul/docker/clarin-dspace/dspace-app"
+IMAGE_BASE_URL="$REGISTRY_BASE/$UFAL_VERSION"
 
-echo "About to build: $REPO_URL"
+echo "About to build: $IMAGE_BASE_URL:$ERCC_VERSION"
 read -p "Press enter to continue"
 
 # export DOCKER_BUILDKIT=1
 docker build \
-    --build-arg LABEL_COMMIT_HASH="$(git rev-parse --short HEAD)" \
-    --build-arg LABEL_VERSION="${UFAL_VERSION}_${ERCC_VERSION}" \
-    --build-arg LABEL_BUILD_DATE="$(date -R)" \
-    -t "$REPO_URL" . -f Dockerfile.ercc
+    --label org.label-schema.version="${UFAL_VERSION}:${ERCC_VERSION}" \
+    --label org.label-schema.build-date="$(date -R)" \
+    --label org.label-schema.vcs-url="$REPO" \
+    --label org.label-schema.registry-overview-url="https://gitlab.inf.unibz.it/commul/docker/clarin-dspace/container_registry" \
+    --label org.label-schema.commit-hash="$(git rev-parse --short HEAD)" \
+    -t "$IMAGE_BASE_URL" . -f Dockerfile.ercc
 
-docker push "$REPO_URL"
+DOCKER_HASH="$(docker image ls "$IMAGE_BASE_URL" --format "{{.ID}}" | head -n1)"
+docker tag "$DOCKER_HASH" "$IMAGE_BASE_URL:$ERCC_VERSION"
+docker push "$IMAGE_BASE_URL:$ERCC_VERSION"
 
-echo "Make sure to change Dockerfile.dspace to point to: $REPO_URL"
+echo "Make sure to change Dockerfile.dspace to point to: $IMAGE_URL"
 echo "$VERSION" > .version
